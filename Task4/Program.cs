d0c26c4bf76829feeca0ebe48b2b1150958d8d31
SoraPay <laichuks@yandex.ru>
2024-02-04 16:49:57 +0300

    add lesson-1:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 1;
+int b = 4;
+int c = 2;
+int d = 13;
+int e = 5;
+
+int max = a;
+
+if(b>max) max = b;
+if(c>max) max = c;
+if(d>max) max = d;
+if(e>max) max = e;
+System.Console.WriteLine(max);
